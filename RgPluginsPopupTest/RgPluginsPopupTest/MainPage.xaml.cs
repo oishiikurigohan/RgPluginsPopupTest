1aef0bbbdf6889b0b7e2860536812bd9645426de
kurigohan <kurigohan_blog-contact@yahoo.co.jp>
2018-01-29 01:17:34 +0900

    Initial commit

---
+++ RgPluginsPopupTest/RgPluginsPopupTest/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Rg.Plugins.Popup.Services;
+using Reactive.Bindings;
+using Xamarin.Forms;
+
+namespace RgPluginsPopupTest
+{
+	public partial class MainPage : ContentPage
+	{
+        public ReactiveCommand AlertCommand { get; } = new ReactiveCommand();
+        public ReactiveCommand ActionSheetCommand { get; } = new ReactiveCommand();
+        public ReactiveCommand PopupCommand { get; } = new ReactiveCommand();
+        public ReactiveCommand TransitionCommand { get; } = new ReactiveCommand();
+
+        public MainPage()
+		{
+			InitializeComponent();
+
+            AlertCommand.Subscribe(() => DisplayAlert("タイトル", "メッセージ", "OK"));
+            ActionSheetCommand.Subscribe(() => DisplayActionSheet("選択してね", "キャンセル", "削除", "編集", "コピー", "貼り付け"));
+            PopupCommand.Subscribe(() => PopupNavigation.PushAsync(new PopUp(true, false)));
+            TransitionCommand.Subscribe(() => App.Current.MainPage = new PopUp(false, true));
+        }
+    }
+}
---
+++ RgPluginsPopupTest/RgPluginsPopupTest/PopUp.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Xamarin.Forms;
+using Rg.Plugins.Popup.Services;
+using Rg.Plugins.Popup.Pages;
+using Reactive.Bindings;
+
+namespace RgPluginsPopupTest
+{
+    public partial class PopUp : PopupPage
+    {
+        public ReactiveProperty<bool> IsPopup { get; } = new ReactiveProperty<bool>();
+        public ReactiveProperty<bool> IsPage { get; } = new ReactiveProperty<bool>();
+        public ReactiveProperty<string[]> Items { get; } = new ReactiveProperty<string[]>();
+        public ReactiveProperty<string> ImageTitle { get; } = new ReactiveProperty<string>("栗きんとぅん");
+        public ReactiveProperty<ImageSource> KumaSource { get; } = new ReactiveProperty<ImageSource>("asahi.png");
+        public ReactiveCommand CloseCommand { get; } = new ReactiveCommand();
+        public ReactiveCommand BackCommand { get; } = new ReactiveCommand();
+
+        public PopUp (bool isPopup, bool isPage)
+		{
+			InitializeComponent ();
+
+            CloseCommand.Subscribe(() => PopupNavigation.PopAsync());
+            BackCommand.Subscribe(() => App.Current.MainPage = new MainPage());
+            Items.Value = new string[] { "001 -- ika", "002 -- tako", "003 -- wakame" };
+            this.IsPopup.Value = isPopup;
+            this.IsPage.Value = isPage;
+        }
+
+        // 普通のページとして表示されているとき、デバイスの「戻る」ボタンに対応
+        protected override bool OnBackButtonPressed()
+        {
+            if(IsPage.Value) App.Current.MainPage = new MainPage();
+            return true;
+        }
+    }
+}
